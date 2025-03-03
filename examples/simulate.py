3505ec8928c99a18f941231a1f6083cc566e4ab4
Luc Mcgrady <lucmcgrady@gmail.com>
2025-03-03 13:30:12 +0000

    Added: Better example

--- examples/simulate.py
+++ examples/simulate.py
@@ -2,7 +2,24 @@ from fsrs_rs_python import DEFAULT_PARAMETERS, default_simulator_config, simulat
 
 if __name__ == "__main__":
     config = default_simulator_config()
-    config.learn_span = 10
+    config.learn_span = 50
+    config.learn_limit = 10
 
-    print(simulate(DEFAULT_PARAMETERS, 0.9, config).cost_per_day)
-    print(simulate(DEFAULT_PARAMETERS, 0.9, default_simulator_config()).cost_per_day)
+    simulation_result = simulate(DEFAULT_PARAMETERS, 0.9, config)
+
+    print("Day,\tMemoriszed,\tReview Count,\tLearn Count,\tCost Per Day")
+    print(
+        "\n".join(
+            [
+                ",\t".join(map(lambda a: f"{a:.2f}", [i, *t]))
+                for i, t in enumerate(
+                    zip(
+                        simulation_result.memorized_cnt_per_day,
+                        simulation_result.review_cnt_per_day,
+                        simulation_result.learn_cnt_per_day,
+                        simulation_result.cost_per_day,
+                    )
+                )
+            ]
+        )
+    )
