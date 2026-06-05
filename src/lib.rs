42027316fae700b6ff1e1c9607c1d1c182181bc0
‏Abdullah Mohamed Elsayed) Mansour‏ <ufhuhehhuei8@gmail.com>
2026-06-05 11:23:44 +0300

    Expose upstream FSRS default parameters (#73)

--- src/lib.rs
+++ src/lib.rs
@@ -249,12 +249,6 @@ fn fsrs_rs_python(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<FSRSReview>()?;
     m.add_function(wrap_pyfunction!(simulate, m)?)?;
     m.add_function(wrap_pyfunction!(default_simulator_config, m)?)?;
-    m.add(
-        "DEFAULT_PARAMETERS",
-        [
-            0.40255, 1.18385, 3.173, 15.69105, 7.1949, 0.5345, 1.4604, 0.0046, 1.54575, 0.1192,
-            1.01925, 1.9395, 0.11, 0.29605, 2.2698, 0.2315, 2.9898, 0.51655, 0.6621,
-        ],
-    )?;
+    m.add("DEFAULT_PARAMETERS", fsrs::DEFAULT_PARAMETERS.to_vec())?;
     Ok(())
 }
